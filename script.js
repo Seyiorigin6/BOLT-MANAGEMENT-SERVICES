23e54156fc3040f07c221c7fd604c0faf2aa5003
EniRufai <eniolarufai2510@gmailcom>
2025-11-20 14:07:16 +0100

    added the js file

---
+++ script.js
@@ -0,0 +1,19 @@
+/* PERSON 1 - PUBLIC / AUTH PAGES */
+ 
+// Person 1 code here...
+
+/*PERSON 2 - CUSTOMER ACCOUNT*/
+ 
+// Person 2 code here...
+
+/*PERSON 3 - CUSTOMER RIDE */
+ 
+// Person 3 code here...
+
+/*PERSON 4 - DRIVER*/
+ 
+// Person 4 code here...
+
+/*PERSON 5 - ADMIN */
+ 
+// Person 5 code here...
